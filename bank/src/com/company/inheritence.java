d6bc547c42533b4e4b78b27139af83d5493b8fa1
ArunNagappaNaik <ar12un-->
2019-06-18 22:01:49 +0530

    inheritence

---
+++ bank/src/com/company/inheritence.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class inheritence {    // single level inheritence
+    void m1(){
+        System.out.println("inside m1");
+    }
+
+    public static void main(String[] args) {
+        inheritence obj= new inheritence();
+        obj.m1();
+        obj.hashCode(); // inherit from d default called java.lang.object
+        obj.getClass();
+        child obj2 = new child();
+        obj2.fun2();
+        obj2.fun1(); // inherit from the parent class
+    }
+}
+class parent{
+    void fun1(){
+        System.out.println("inside parent class");
+    }
+}
+class child extends parent{
+    void fun2(){
+        System.out.println("inside child class");
+    }
+}
